1af99e1311f49ec6a04093976050480d6cea85a3
elfox14 <86858355+elfox14@users.noreply.github.com>
2025-09-08 00:44:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = 3000;
 
 // --- إعدادات قاعدة البيانات ---
-mongodb+srv://nfc_db_user:mahMAH123#@@cluster0.tscffw5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0; // الرابط الافتراضي لـ MongoDB
+mongodb+srv://nfc_db_user:mahMAH123%23%40@cluster0.tscffw5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0; // الرابط الافتراضي لـ MongoDB
 const dbName = 'digital_cards_db';
 const collectionName = 'designs';
 let db;
